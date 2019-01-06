40a607f866f50b927dc03ecf57997de464f8cdc4
Tamas Lustyik <elveestei@gmail.com>
2019-01-06 17:37:41 +0100

    Added support for property qualifiers

--- Cameo/CMIO/Property.swift
+++ Cameo/CMIO/Property.swift
@@ -10,6 +10,27 @@ import Foundation
 import CoreMediaIO
 
 
+protocol QualifierProtocol {
+    var data: UnsafeMutableRawPointer { get }
+    var size: Int { get }
+}
+
+class Qualifier<T>: QualifierProtocol {
+    let data: UnsafeMutableRawPointer
+    let size: Int
+    
+    init(from scalar: T) {
+        size = MemoryLayout<T>.size
+        data = UnsafeMutableRawPointer.allocate(byteCount: size, alignment: MemoryLayout<T>.alignment)
+        let typedData = data.bindMemory(to: T.self, capacity: 1)
+        typedData.pointee = scalar
+    }
+    
+    deinit {
+        data.deallocate()
+    }
+}
+
 enum PropertyType {
     enum Kind {
         case pod, podArray, cf, cfArray, function
@@ -46,14 +67,18 @@ enum PropertyType {
         }
     }
     
-    static func podTypeValue<T>(for selector: CMIOObjectPropertySelector, in objectID: CMIOObjectID) -> T? {
+    static func podTypeValue<T>(for selector: CMIOObjectPropertySelector,
+                                qualifiedBy qualifier: QualifierProtocol? = nil,
+                                in objectID: CMIOObjectID) -> T? {
         var address = CMIOObjectPropertyAddress(selector)
         var dataSize: UInt32 = UInt32(MemoryLayout<T>.size)
         var dataUsed: UInt32 = 0
         var data = UnsafeMutableRawPointer.allocate(byteCount: Int(dataSize), alignment: MemoryLayout<T>.alignment)
         defer { data.deallocate() }
         
-        let status = CMIOObjectGetPropertyData(objectID, &address, 0, nil, dataSize, &dataUsed, data)
+        let status = CMIOObjectGetPropertyData(objectID, &address,
+                                               UInt32(qualifier?.size ?? 0), qualifier?.data,
+                                               dataSize, &dataUsed, data)
         guard status == 0 else {
             return nil
         }
@@ -62,11 +87,15 @@ enum PropertyType {
         return typedData.pointee
     }
 
-    static func podArrayTypeValue<T>(for selector: CMIOObjectPropertySelector, in objectID: CMIOObjectID) -> [T]? {
+    static func podArrayTypeValue<T>(for selector: CMIOObjectPropertySelector,
+                                     qualifiedBy qualifier: QualifierProtocol? = nil,
+                                     in objectID: CMIOObjectID) -> [T]? {
         var address = CMIOObjectPropertyAddress(selector)
         var dataSize: UInt32 = 0
         
-        var status = CMIOObjectGetPropertyDataSize(objectID, &address, 0, nil, &dataSize)
+        var status = CMIOObjectGetPropertyDataSize(objectID, &address,
+                                                   UInt32(qualifier?.size ?? 0), qualifier?.data,
+                                                   &dataSize)
         guard status == 0 else {
             return nil
         }
@@ -76,7 +105,9 @@ enum PropertyType {
         var data = UnsafeMutableRawPointer.allocate(byteCount: Int(dataSize), alignment: MemoryLayout<T>.alignment)
         defer { data.deallocate() }
         
-        status = CMIOObjectGetPropertyData(objectID, &address, 0, nil, dataSize, &dataUsed, data)
+        status = CMIOObjectGetPropertyData(objectID, &address,
+                                           UInt32(qualifier?.size ?? 0), qualifier?.data,
+                                           dataSize, &dataUsed, data)
         guard status == 0 else {
             return nil
         }
@@ -85,14 +116,18 @@ enum PropertyType {
         return UnsafeBufferPointer<T>(start: typedData, count: count).map { $0 }
     }
 
-    static func cfTypeValue<T>(for selector: CMIOObjectPropertySelector, in objectID: CMIOObjectID) -> T? {
+    static func cfTypeValue<T>(for selector: CMIOObjectPropertySelector,
+                               qualifiedBy qualifier: QualifierProtocol? = nil,
+                               in objectID: CMIOObjectID) -> T? {
         var address = CMIOObjectPropertyAddress(selector)
         var dataSize: UInt32 = UInt32(MemoryLayout<CFTypeRef>.size)
         var dataUsed: UInt32 = 0
         var data = UnsafeMutableRawPointer.allocate(byteCount: Int(dataSize), alignment: MemoryLayout<CFTypeRef>.alignment)
         defer { data.deallocate() }
         
-        let status = CMIOObjectGetPropertyData(objectID, &address, 0, nil, dataSize, &dataUsed, data)
+        let status = CMIOObjectGetPropertyData(objectID, &address,
+                                               UInt32(qualifier?.size ?? 0), qualifier?.data,
+                                               dataSize, &dataUsed, data)
         guard status == 0 else {
             return nil
         }
@@ -101,11 +136,15 @@ enum PropertyType {
         return typedData.pointee as? T
     }
 
-    static func cfArrayTypeValue<T>(for selector: CMIOObjectPropertySelector, in objectID: CMIOObjectID) -> [T]? {
+    static func cfArrayTypeValue<T>(for selector: CMIOObjectPropertySelector,
+                                    qualifiedBy qualifier: QualifierProtocol? = nil,
+                                    in objectID: CMIOObjectID) -> [T]? {
         var address = CMIOObjectPropertyAddress(selector)
         var dataSize: UInt32 = 0
         
-        var status = CMIOObjectGetPropertyDataSize(objectID, &address, 0, nil, &dataSize)
+        var status = CMIOObjectGetPropertyDataSize(objectID, &address,
+                                                   UInt32(qualifier?.size ?? 0), qualifier?.data,
+                                                   &dataSize)
         guard status == 0 else {
             return nil
         }
@@ -115,7 +154,9 @@ enum PropertyType {
         var data = UnsafeMutableRawPointer.allocate(byteCount: Int(dataSize), alignment: MemoryLayout<CFTypeRef>.alignment)
         defer { data.deallocate() }
         
-        status = CMIOObjectGetPropertyData(objectID, &address, 0, nil, dataSize, &dataUsed, data)
+        status = CMIOObjectGetPropertyData(objectID, &address,
+                                           UInt32(qualifier?.size ?? 0), qualifier?.data,
+                                           dataSize, &dataUsed, data)
         guard status == 0 else {
             return nil
         }
@@ -159,27 +200,33 @@ extension PropertySet {
 
 
 enum Property {
-    static func value<S, T>(of property: S, in objectID: CMIOObjectID) -> T? where S: PropertySet {
+    static func value<S, T>(of property: S,
+                            qualifiedBy qualifier: QualifierProtocol? = nil,
+                            in objectID: CMIOObjectID) -> T? where S: PropertySet {
         let desc = S.descriptors[property]!
         switch desc.type.kind {
-        case .pod: return PropertyType.podTypeValue(for: desc.selector, in: objectID)
-        case .cf: return PropertyType.cfTypeValue(for: desc.selector, in: objectID)
+        case .pod: return PropertyType.podTypeValue(for: desc.selector, qualifiedBy: qualifier, in: objectID)
+        case .cf: return PropertyType.cfTypeValue(for: desc.selector, qualifiedBy: qualifier, in: objectID)
+        default: return nil
+        }
+    }
+
+    static func arrayValue<S, T>(of property: S,
+                                 qualifiedBy qualifier: QualifierProtocol? = nil,
+                                 in objectID: CMIOObjectID) -> [T]? where S: PropertySet {
+        let desc = S.descriptors[property]!
+        switch desc.type.kind {
+        case .podArray: return PropertyType.podArrayTypeValue(for: desc.selector, qualifiedBy: qualifier, in: objectID)
+        case .cfArray: return PropertyType.cfArrayTypeValue(for: desc.selector, qualifiedBy: qualifier, in: objectID)
         default: return nil
         }
     }
     
-    static func arrayValue<S, T>(of property: S, in objectID: CMIOObjectID) -> [T]? where S: PropertySet {
+    static func description<S>(of property: S,
+                               qualifiedBy qualifier: QualifierProtocol? = nil,
+                               in objectID: CMIOObjectID) -> String? where S: PropertySet {
         let desc = S.descriptors[property]!
-        switch desc.type.kind {
-        case .podArray: return PropertyType.podArrayTypeValue(for: desc.selector, in: objectID)
-        case .cfArray: return PropertyType.cfArrayTypeValue(for: desc.selector, in: objectID)
-        default: return nil
-        }
-    }
-    
-    static func description<S>(of property: S, in objectID: CMIOObjectID) -> String? where S: PropertySet {
-        let desc = S.descriptors[property]!
-        return propertyDescription(for: desc.selector, ofType: desc.type, in: objectID)
+        return propertyDescription(for: desc.selector, ofType: desc.type, qualifiedBy: qualifier, in: objectID)
     }
     
     static func isSettable<S>(_ property: S, in objectID: CMIOObjectID) -> Bool where S: PropertySet {
@@ -228,80 +275,83 @@ func fourCCDescription(from value: UInt32) -> String? {
 }
 
 
-func propertyDescription(for selector: CMIOObjectPropertySelector, ofType type: PropertyType, in objectID: CMIOObjectID) -> String? {
+func propertyDescription(for selector: CMIOObjectPropertySelector,
+                         ofType type: PropertyType,
+                         qualifiedBy qualifier: QualifierProtocol? = nil,
+                         in objectID: CMIOObjectID) -> String? {
     switch type {
     case .boolean:
-        if let value: DarwinBoolean = PropertyType.podTypeValue(for: selector, in: objectID) {
+        if let value: DarwinBoolean = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "\(value)"
         }
     case .boolean32:
-        if let value: UInt32 = PropertyType.podTypeValue(for: selector, in: objectID) {
+        if let value: UInt32 = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return value != 0 ? "true (\(value))" : "false (0)"
         }
     case .int32, .uint32:
-        if let value: UInt32 = PropertyType.podTypeValue(for: selector, in: objectID) {
+        if let value: UInt32 = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "\(value)"
         }
     case .uint64:
-        if let value: UInt64 = PropertyType.podTypeValue(for: selector, in: objectID) {
+        if let value: UInt64 = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "\(value)"
         }
     case .float32:
-        if let value: Float32 = PropertyType.podTypeValue(for: selector, in: objectID) {
+        if let value: Float32 = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "\(value)"
         }
     case .float64:
-        if let value: Float64 = PropertyType.podTypeValue(for: selector, in: objectID) {
+        if let value: Float64 = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "\(value)"
         }
     case .fourCC, .classID:
-        if let value: CMIOClassID = PropertyType.podTypeValue(for: selector, in: objectID) {
+        if let value: CMIOClassID = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             if let fcc = fourCCDescription(from: value) {
                 return "\(fcc)"
             }
             return "\(value)"
         }
     case .objectID, .deviceID:
-        if let value: CMIOObjectID = PropertyType.podTypeValue(for: selector, in: objectID) {
+        if let value: CMIOObjectID = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return value != kCMIOObjectUnknown ? "@\(value)" : "<null>"
         }
     case .audioValueTranslation:
         return "<function>"
     case .audioValueRange:
-        if let value: AudioValueRange = PropertyType.podTypeValue(for: selector, in: objectID) {
+        if let value: AudioValueRange = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "AudioValueRange {\(value.mMinimum), \(value.mMaximum)}"
         }
     case .propertyAddress:
-        if let value: CMIOObjectPropertyAddress = PropertyType.podTypeValue(for: selector, in: objectID) {
+        if let value: CMIOObjectPropertyAddress = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "CMIOObjectPropertyAddress {\(fourCCDescription(from: value.mSelector)!), " +
             "\(fourCCDescription(from: value.mScope)!), \(fourCCDescription(from: value.mElement)!)"
         }
     case .streamConfiguration:
-        if let value: CMIODeviceStreamConfiguration = PropertyType.podTypeValue(for: selector, in: objectID) {
+        if let value: CMIODeviceStreamConfiguration = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "CMIODeviceStreamConfiguration {\(value.mNumberStreams)}"
         }
     case .pid:
-        if let value: pid_t = PropertyType.podTypeValue(for: selector, in: objectID) {
+        if let value: pid_t = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "\(value)"
         }
-        //    case .componentDescription:
-        //        if let value: ComponentDescription = PropertyType.podTypeValue(for: selector, in: objectID) {
-        //            return "\(value)"
-    //        }
+//    case .componentDescription:
+//        if let value: ComponentDescription = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
+//            return "\(value)"
+//        }
     case .time:
-        if let value: CMTime = PropertyType.podTypeValue(for: selector, in: objectID) {
+        if let value: CMTime = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "CMTime {\(value.value) / \(value.timescale)}"
         }
     case .cgRect:
-        if let value: CGRect = PropertyType.podTypeValue(for: selector, in: objectID) {
+        if let value: CGRect = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "CGRect {{\(value.origin.x), \(value.origin.y)}, {\(value.size.width), \(value.size.height)}}"
         }
     case .streamDeck:
-        if let value: CMIOStreamDeck = PropertyType.podTypeValue(for: selector, in: objectID) {
+        if let value: CMIOStreamDeck = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "CMIOStreamDeck {\(value.mStatus), \(value.mState), \(value.mState2)}"
         }
     case .smpteCallback:
-        if let value: CMIODeviceSMPTETimeCallback = PropertyType.podTypeValue(for: selector, in: objectID) {
+        if let value: CMIODeviceSMPTETimeCallback = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             if value.mGetSMPTETimeProc != nil {
                 let ctx = value.mRefCon != nil ? "\(value.mRefCon!)" : "0x0"
                 return "CMIODeviceSMPTETimeCallback {proc=\(value.mGetSMPTETimeProc!), ctx=\(ctx)}"
@@ -311,7 +361,7 @@ func propertyDescription(for selector: CMIOObjectPropertySelector, ofType type: 
             }
         }
     case .scheduledOutputCallback:
-        if let value: CMIOStreamScheduledOutputNotificationProcAndRefCon = PropertyType.podTypeValue(for: selector, in: objectID) {
+        if let value: CMIOStreamScheduledOutputNotificationProcAndRefCon = PropertyType.podTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             if value.scheduledOutputNotificationProc != nil {
                 let ctx = value.scheduledOutputNotificationRefCon != nil ? "\(value.scheduledOutputNotificationRefCon!)" : "0x0"
                 return "CMIOStreamScheduledOutputNotificationProcAndRefCon {proc=\(value.scheduledOutputNotificationProc!), ctx=\(ctx)"
@@ -322,41 +372,41 @@ func propertyDescription(for selector: CMIOObjectPropertySelector, ofType type: 
         }
         
     case .arrayOfUInt32s:
-        if let value: [UInt32] = PropertyType.podArrayTypeValue(for: selector, in: objectID) {
+        if let value: [UInt32] = PropertyType.podArrayTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "\(value)"
         }
     case .arrayOfFloat64s:
-        if let value: [Float64] = PropertyType.podArrayTypeValue(for: selector, in: objectID) {
+        if let value: [Float64] = PropertyType.podArrayTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "\(value)"
         }
     case .arrayOfDeviceIDs, .arrayOfObjectIDs, .arrayOfStreamIDs:
-        if let value: [CMIOObjectID] = PropertyType.podArrayTypeValue(for: selector, in: objectID) {
+        if let value: [CMIOObjectID] = PropertyType.podArrayTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "[" + value.map { $0 != kCMIOObjectUnknown ? "@\($0)" : "<null>" }.joined(separator: ", ") + "]"
         }
     case .arrayOfAudioValueRanges:
-        if let value: [AudioValueRange] = PropertyType.podArrayTypeValue(for: selector, in: objectID) {
+        if let value: [AudioValueRange] = PropertyType.podArrayTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "[" + value.map { "AudioValueRange {\($0.mMinimum), \($0.mMaximum)}" }.joined(separator: ", ") + "]"
         }
         
     case .string:
-        if let value: CFString = PropertyType.cfTypeValue(for: selector, in: objectID) {
+        if let value: CFString = PropertyType.cfTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "\(value)"
         }
     case .formatDescription:
-        if let value: CMFormatDescription = PropertyType.cfTypeValue(for: selector, in: objectID) {
+        if let value: CMFormatDescription = PropertyType.cfTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "\(value)"
         }
     case .sampleBuffer:
-        if let value: CMSampleBuffer = PropertyType.cfTypeValue(for: selector, in: objectID) {
+        if let value: CMSampleBuffer = PropertyType.cfTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "\(value)"
         }
     case .clock:
-        if let value: CFTypeRef = PropertyType.cfTypeValue(for: selector, in: objectID) {
+        if let value: CFTypeRef = PropertyType.cfTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "\(value)"
         }
         
     case .arrayOfFormatDescriptions:
-        if let value: [CMFormatDescription] = PropertyType.cfArrayTypeValue(for: selector, in: objectID) {
+        if let value: [CMFormatDescription] = PropertyType.cfArrayTypeValue(for: selector, qualifiedBy: qualifier, in: objectID) {
             return "[" + value.map { "\($0)" }.joined(separator: ", ") + "]"
         }
         
